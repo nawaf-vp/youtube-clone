b2fe39fc43e5acf0661092bf5c2b58112f24cd2f
Nawaf v p <nawafvp.dev@gmail.com>
2023-05-04 23:27:21 +0530

    corrected

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { Typography ,Card,CardContent,CardMedia} from "@mui/material";
-import { ChangeCircle, Height } from "@mui/icons-material";
+import { ChangeCircle, CheckCircle, Height } from "@mui/icons-material";
 import { demoThumbnailUrl ,demoVideoUrl,demoVideoTitle,demoChannelTitle,demoChannelUrl} from "../utils/constants";
 
 
@@ -8,7 +8,8 @@ const VideoCard = ({video:{id :{videoId} ,snippet}}) => {
     
 
   return (
-    <Card>
+    <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0
+      }}>
         <Link to ={videoId ? `/video/${videoId}`:demoVideoUrl}>
            <CardMedia
            image={snippet?.thumbnails?.high?.url}
@@ -16,8 +17,25 @@ const VideoCard = ({video:{id :{videoId} ,snippet}}) => {
            sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }}
            />
         </Link>
+        <CardContent sx={{backgroundColor :'#1e1e1e',height:'106px'}}>
+
+        <Link to ={videoId ? `/video/${videoId}`:demoVideoUrl}>
+           <Typography variant="subtitle1"
+                        fontWeight="bold" color={'#fff'}>
+            {snippet?.title.slice(0,60)|| demoVideoTitle.slice(0,60)}
+           </Typography>
+        </Link>
+
+        <Link to ={snippet?.channelId ? `/channel/${snippet?.channelId}`:demoChannelUrl}>
+           <Typography variant="subtitle2"
+                        fontWeight="bold" color={'grey'}>
+            {snippet?.channelTitle|| demoChannelTitle}
+            <CheckCircle sx={{fontSize:12, color:'gray',ml:'5px'}}/>
+           </Typography>
+        </Link>
+        </CardContent>
     </Card>
   )
-}
+}  
 
 export default VideoCard
